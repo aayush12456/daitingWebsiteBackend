3e89a58cc16adf714c1e03631efee12f6d48aa4a
Aayush <aayushtapadia28@gmail.com>
2024-08-05 15:35:32 +0530

    fifteen commit

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,12 @@ const mongoose=require('mongoose')
 // }).catch(()=>{
 //     console.log("unable to connect ")
 // })
-mongoose.connect("mongodb+srv://apnapan96:12345@apnapancluster.dmqxm67.mongodb.net/ApnaPanDatabase?retryWrites=true&w=majority&appName=ApnaPanCluster").then(()=>{
+mongoose.connect("mongodb+srv://apnapan96:12345@apnapancluster.dmqxm67.mongodb.net/ApnaPanDatabase?retryWrites=true&w=majority&appName=ApnaPanCluster",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Increase the server selection timeout
+    socketTimeoutMS: 45000 
+}).then(()=>{
     console.log("Database connected successfully")
 }).catch(()=>{
     console.log("unable to connect ")
