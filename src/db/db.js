9170798e2dc49b7d3e7d00c207c5d3803474f447
Aayush <aayushtapadia28@gmail.com>
2024-08-05 16:39:57 +0530

    nineteen commit

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,13 @@
 const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config()
 // mongodbCompass local database configuration
 // mongoose.connect("mongodb://127.0.0.1/dateApplication").then(()=>{
 //     console.log("Database connected successfully")
 // }).catch(()=>{
 //     console.log("unable to connect ")
 // })
-mongoose.connect("mongodb+srv://apnapan96:12345@apnapancluster.dmqxm67.mongodb.net/ApnaPanDatabase?retryWrites=true&w=majority&appName=ApnaPanCluster").then(()=>{
+mongoose.connect(process.env.MONGODB_CONNECT_URL).then(()=>{
     console.log("Database connected successfully")
 }).catch(()=>{
     console.log("unable to connect ")
